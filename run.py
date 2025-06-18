a3703a30a701f7601b7290cfb719a18049b7e7e6
Troy314 <hectorpillot@gmail.com>
2025-06-18 12:31:27 +0200

    change row value et correct resource formula

--- run.py
+++ run.py
@@ -26,6 +26,9 @@ def csv_to_md(csv_filename, output_dir):
             if begin !="" and int(row[0]) < int(begin):
                 continue # Skip stating project
 
+            if row[23]!="OK":
+                continue #Skip not "OK" status
+
             if row[0]!="":
                 ID = row[0]
             else :
@@ -63,8 +66,8 @@ def csv_to_md(csv_filename, output_dir):
                 ##############
 
                 mdfile.write(f"\n## Abstract\n{row[5]}") # Write the abstract
-                if row[12]!="":
-                    mdfile.write(f"\n\n[{row[12]} ({row[13]})]({row[14]})") # Write the url of the presentation slides as a hyperlink with [Conference (date)] as text
+                if row[13]!="":
+                    mdfile.write(f"\n\n[{row[11]} ({row[12]})]({row[13]})") # Write the url of the presentation slides as a hyperlink with [Conference (date)] as text
                 mdfile.write(f"\n\n---\n")
 
                 #############
@@ -82,11 +85,11 @@ def csv_to_md(csv_filename, output_dir):
                 #mdfile.write(f"\n\n")
 
                 mdfile.write(f"<b>Related IPPOG Collaboration member :</b>\n") # Write IPPOG members as hyperlink to the related ippog.org page
-                contact = next(iter({row[8]})).split(', ')
+                contact = next(iter({row[19]})).split(', ')
                 for i in range (len(contact)):
                     mdfile.write(f"- [{contact[i]}]({members_dico[contact[i]]})\n") # Call the URL from the dictionary
 
-                mdfile.write(f"\n<b>Contact :</b>\n- {row[9]}".replace('@',' [at] ')) # Protect email by replacing @ with [at]
+                mdfile.write(f"\n<b>Contact :</b>\n- {row[8]}".replace('@',' [at] ')) # Protect email by replacing @ with [at]
 
                 mdfile.write(f"\n\n---\n")
 
@@ -94,7 +97,7 @@ def csv_to_md(csv_filename, output_dir):
                 ## Status ##
                 ############
 
-                mdfile.write(f"\n## Project status\n{row[11]} (updated {datetime.today().strftime('%Y-%m-%d')})") # Write the project status at today's date
+                mdfile.write(f"\n## Project status\n{row[10]} (updated {datetime.today().strftime('%Y-%m-%d')})") # Write the project status at today's date
 
                 mdfile.write(f"\n\n---\n")
 
@@ -102,12 +105,13 @@ def csv_to_md(csv_filename, output_dir):
                 ## Ressoucres ##
                 ################
 
-                ressource = next(iter({row[15]})).split('\n') # Separate ressources and write them as a list of hyperlink
+                ressource = next(iter({row[14]})).split('\n') # Separate ressources and write them as a list of hyperlink
                 if(ressource!=['']):
                     mdfile.write(f"\n## Files & Resources")
 
                     for j in range (len(ressource)):
-                        mdfile.write(f"\n- [{ressource[j].split(':')[0]}]({ressource[j].split(':')[1]})")
+                        if ressource[j]!="":
+                            mdfile.write(f"\n- [{ressource[j].replace(' ','').split(':http')[0]}](http{ressource[j].replace(' ','').split(':http')[1]})")
                     mdfile.write(f"\n\n---\n")
 
                 ######################
@@ -123,8 +127,8 @@ def csv_to_md(csv_filename, output_dir):
                 ### Should not appear on the website, the PROJECT-ID should be used for the URL slug, Categories and Tags sould be added to the corresponding menu
                 mdfile.write(f"\n\n<TODO Manually add the categories and tags then remove everything bellow>")
                 mdfile.write(f"\nPROJECT-{ID}")
-                mdfile.write(f"\nCategories : {row[18]} / {row[19]} / {row[17]} / {row[16]}")
-                mdfile.write(f"\nTags : {row[20]} / {row[21]} / {row[22]}")
+                mdfile.write(f"\nCategories : {row[17]} / {row[18]} / {row[16]} / {row[15]}")
+                mdfile.write(f"\nTags : {row[20]} / {row[21]}")
 
             print(f"Created: {filepath}")
 
